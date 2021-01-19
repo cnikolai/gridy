e7bd8f096dd1f3ec0123d804d0dbabfcc4c4c6ec
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-01-19 08:47:36 -0500

    ImageEditorViewController working getting gestures working

--- Gridy/Controller/ImageEditorViewController.swift
+++ Gridy/Controller/ImageEditorViewController.swift
@@ -10,9 +10,10 @@ import UIKit
 import Photos
 import AVFoundation
 
-class ImageEditorViewController: UIViewController  {
+class ImageEditorViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UIGestureRecognizerDelegate  {
 
     var creation:Creation!
+    var initialImageViewOffset = CGPoint()
     var topCellItems: [String] = [
         "Boats",
         "Car",
@@ -26,8 +27,29 @@ class ImageEditorViewController: UIViewController  {
     
     
     func configure() {
-        unsolvedPuzzle.backgroundColor = .red
+        unsolvedPuzzle.backgroundColor = .clear
+        unsolvedPuzzle.image = creation.image
         view.addSubview(unsolvedPuzzle)
+        
+        unsolvedPuzzle.isUserInteractionEnabled = true
+
+      // create tap gesture recognizer
+      //let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(changeImage(_:)))
+      //tapGestureRecognizer.delegate = self
+      //unsolvedPuzzle.addGestureRecognizer(tapGestureRecognizer)
+
+      let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(moveImageView(_:)))
+      //panGestureRecognizer.delegate = self
+      unsolvedPuzzle.addGestureRecognizer(panGestureRecognizer)
+
+      let rotationGestureRecognizer = UIRotationGestureRecognizer(target: self, action: #selector(rotateImageView(_:)))
+      //rotationGestureRecognizer.delegate = self
+      unsolvedPuzzle.addGestureRecognizer(rotationGestureRecognizer)
+
+      let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(scaleImageView(_:)))
+      //pinchGestureRecognizer.delegate = self
+      unsolvedPuzzle.addGestureRecognizer(pinchGestureRecognizer)
+
     }
     
     override func viewDidLoad() {
@@ -35,5 +57,48 @@ class ImageEditorViewController: UIViewController  {
         // Do additional setup after loading the view
         configure()
     }
-
+    
+    @objc func moveImageView(_ sender: UIPanGestureRecognizer) {
+               let translation = sender.translation(in: unsolvedPuzzle.superview)
+    
+               if sender.state == .began {
+                   initialImageViewOffset = unsolvedPuzzle.frame.origin
+               }
+    
+               let position = CGPoint(x: translation.x + initialImageViewOffset.x - unsolvedPuzzle.frame.origin.x, y: translation.y + initialImageViewOffset.y - unsolvedPuzzle.frame.origin.y)
+    
+               unsolvedPuzzle.transform = unsolvedPuzzle.transform.translatedBy(x: position.x, y: position.y)
+           }
+    
+           @objc func rotateImageView(_ sender: UIRotationGestureRecognizer) {
+               unsolvedPuzzle.transform = unsolvedPuzzle.transform.rotated(by: sender.rotation)
+               sender.rotation = 0
+           }
+    
+           @objc func scaleImageView(_ sender: UIPinchGestureRecognizer) {
+               unsolvedPuzzle.transform = unsolvedPuzzle.transform.scaledBy(x: sender.scale, y: sender.scale)
+               sender.scale = 1
+           }
+           
+           func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
+                                  shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer)
+               -> Bool {
+    
+                   // simultaneous gesture recognition will only be supported for LocalPicture
+                   if gestureRecognizer.view != unsolvedPuzzle {
+                       return false
+                   }
+    
+                   // neither of the recognized gestures should not be tap gesture
+                   if gestureRecognizer is UITapGestureRecognizer
+                       || otherGestureRecognizer is UITapGestureRecognizer
+                       || gestureRecognizer is UIPanGestureRecognizer
+                       || otherGestureRecognizer is UIPanGestureRecognizer {
+                       return false
+                   }
+    
+                   return true
+           }
+    
+   
 }
